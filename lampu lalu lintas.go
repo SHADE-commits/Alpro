4a35b1776e6983bdbc7584a2a5d1f6db42717119
Shadeee <nnovtiera@gmail.com>
2025-12-17 03:09:31 +0700

    Add files via upload

--- lampu lalu lintas.go
+++ lampu lalu lintas.go
@@ -12,16 +12,16 @@ func main() {
 		switch currentLight {
 		case "RED":
 			fmt.Println("Lampu Merah: Berhenti")
-			time.Sleep(20 * time.Second)
+			time.Sleep(30 * time.Second)
 			currentLight = "GREEN"
-		case "YELLOW":
-			fmt.Println("Lampu Kuning: Siap-siap")
-			time.Sleep(20 * time.Second)
-			currentLight = "RED"
 		case "GREEN":
 			fmt.Println("Lampu Hijau: Jalan")
-			time.Sleep(5 * time.Second)
+			time.Sleep(30 * time.Second)
 			currentLight = "YELLOW"
+		case "YELLOW":
+			fmt.Println("Lampu Kuning: Siap-siap")
+			time.Sleep(5 * time.Second)
+			currentLight = "RED"
 		}
 	}
 }
