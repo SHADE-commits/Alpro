0405c0a9f8a7f5421a37a4dfc0a6f5c075e87a83
Shadeee <nnovtiera@gmail.com>
2026-06-12 23:42:55 +0700

    Update candidate.go

--- voting.bem/candidate.go
+++ voting.bem/candidate.go
@@ -2,23 +2,25 @@ package main
 
 import "fmt"
 
+const maxCandidate = 20
+
 type Candidate struct {
 	Name  string
 	Votes int
 }
 
-var candidates []Candidate
+var candidates [maxCandidate]Candidate
+var candidateCount int
 
 func addCandidate() {
 	var name string
 
-	fmt.Print("Candidate name: ")
+	fmt.Print("Enter candidate name: ")
 	fmt.Scan(&name)
 
-	candidates = append(candidates, Candidate{
-		Name:  name,
-		Votes: 0,
-	})
+	candidates[candidateCount].Name = name
+	candidates[candidateCount].Votes = 0
+	candidateCount++
 
 	fmt.Println("Candidate added successfully")
 }
@@ -26,7 +28,24 @@ func addCandidate() {
 func showCandidates() {
 	fmt.Println("\n=== Candidate List ===")
 
-	for i, c := range candidates {
-		fmt.Println(i+1, "-", c.Name, "| Votes:", c.Votes)
+	for i := 0; i < candidateCount; i++ {
+		fmt.Println(i+1, "-", candidates[i].Name, "| Votes:", candidates[i].Votes)
 	}
+}
+
+func deleteCandidate() {
+	var number int
+
+	showCandidates()
+
+	fmt.Print("Enter candidate number to delete: ")
+	fmt.Scan(&number)
+
+	for i := number - 1; i < candidateCount-1; i++ {
+		candidates[i] = candidates[i+1]
+	}
+
+	candidateCount--
+
+	fmt.Println("Candidate deleted successfully")
 }
