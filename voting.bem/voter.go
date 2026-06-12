210ad10180f7c279cfa6470a8bfd24a177a0994a
Shadeee <nnovtiera@gmail.com>
2026-06-12 23:44:48 +0700

    Update voter.go

--- voting.bem/voter.go
+++ voting.bem/voter.go
@@ -1,39 +1,11 @@
 package main
 
-import "fmt"
+const maxVoter = 100
 
 type Voter struct {
 	NIM      string
-	Name     string
 	HasVoted bool
 }
 
-var voters []Voter
-
-func addVoter() {
-	var nim, name string
-
-	fmt.Print("Enter NIM: ")
-	fmt.Scan(&nim)
-
-	fmt.Print("Enter Candidate Name: ")
-	fmt.Scan(&name)
-
-	voters = append(voters, Voter{
-		NIM:      nim,
-		Name:     name,
-		HasVoted: false,
-	})
-
-	fmt.Println("Voter added successfully")
-}
-
-func findVoter(nim string) int {
-	for i, v := range voters {
-		if v.NIM == nim {
-			return i
-		}
-	}
-
-	return -1
-}
+var voters [maxVoter]Voter
+var voterCount int
